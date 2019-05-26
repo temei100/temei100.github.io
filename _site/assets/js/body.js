870baba67684a38e34e4800e3d135f1cfa0b9ea0
lijiaocn <lijiaocn@foxmail.com>
2019-05-26 17:03:29 +0800

    m

--- _site/assets/js/body.js
+++
@@ -1,5 +0,0 @@
-/*点击后，收起nav下拉列表*/
-$('#navbarcolumn a').on('click', function(){
-	$('.btn-navbar').click(); //bootstrap 2.x
-	$('.navbar-toggle').click() //bootstrap 3.x by Richard
-});
--- _site/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# run.sh
-# Copyright (C) 2018 lijiaocn <lijiaocn@foxmail.com>
-#
-# Distributed under terms of the GPL license.
-#
-
-nohup bundle exec jekyll serve --port 3000 2>&1 1 > /tmp/jekyll8.log &
